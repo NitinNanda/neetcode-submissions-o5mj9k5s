db5255aba7adaa1eaa8f4bbe2d332d1e78eb1f0f
Nitin Nanda <nitinnanda11@gmail.com>
2026-05-29 14:56:55 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' ||c == '[') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else {
+                if(isMatch(stack.peek(), c)) {
+                    stack.pop();
+                } else return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    private boolean isMatch(char c1, char c2) {
+        return (c1=='(' && c2 == ')') ||
+        (c1=='[' && c2 == ']') ||
+        (c1=='{' && c2 == '}');
+    }
+
+
+    // Stack<Character> stack = new Stack<>();
+        // for(char c : s.toCharArray()) {
+        //     if(c == '(' || c == '{' || c == '['){
+        //         stack.push(c);
+        //     } else if(stack.isEmpty()){
+        //         return false;
+        //     } else {
+        //         if(isMatch(stack.peek(), c)) {
+        //             stack.pop();
+        //         } else {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return stack.isEmpty();
+
+    // public boolean isMatch(char c1, char c2){
+    //     return (c1=='(' && c2 == ')') 
+    //         || (c1=='{' && c2 == '}') 
+    //         || (c1=='[' && c2 == ']');
+    // }
+}
