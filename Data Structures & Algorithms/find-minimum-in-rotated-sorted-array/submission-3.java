691fc83c5e9e52726d133a25022007b9843abce3
Nitin Nanda <nitinnanda11@gmail.com>
2026-05-29 17:57:15 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length-1, min = Integer.MAX_VALUE;
+        while(l<r){
+            int m = l + (r-l)/2;
+            if(nums[m] > nums[r]){
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return nums[l];
+
+
+
+        // int l = 0, r = nums.length - 1;
+        //  while(l < r) {
+        //     int m = l + (r-l)/2;
+        //     if(nums[m]> nums[r]) {
+        //         l = m+1;
+        //     } else {
+        //         r = m;
+        //     }
+        //  }
+        // return nums[l];
+    }
+}
