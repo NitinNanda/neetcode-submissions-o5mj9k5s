390fe4ea67f744b97ba5da193dad64b45a92d5ad
Nitin Nanda <nitinnanda11@gmail.com>
2026-07-12 19:37:08 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = 0;
+        for(int pile : piles) {
+            r = Math.max(r , pile);
+        }
+        int res = r;
+        while(l<=r) {
+            long totalTime = 0;
+            int m = l + (r-l)/2;
+            for(int p : piles) {
+                totalTime += Math.ceil((double)p/m);
+            } 
+            if(totalTime <= h){
+                res = m;
+                r = m-1;
+            } else {
+                l = m +1;
+            }
+        }
+        return res;
+    }
+}
