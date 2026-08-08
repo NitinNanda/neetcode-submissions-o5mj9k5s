a0fce07a8881ebd9cc3f179537b84034fdd5dfef
Nitin Nanda <nitinnanda11@gmail.com>
2026-08-08 19:41:31 -0400

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,8 @@
+select s.seller_name
+from seller s
+where s.seller_id not in (
+    select seller_id from
+    orders o
+    WHERE sale_date >= '2020-01-01' AND sale_date <= '2020-12-31'
+)
+order by s.seller_name
